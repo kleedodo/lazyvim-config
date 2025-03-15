3302a1af0fffbf2a42cc057e777054fa1f668eee
kleedodo <kleedodo@duck.com>
2025-03-15 20:42:10 +0800

    formart

--- lua/plugins/extern_tools.lua
+++ lua/plugins/extern_tools.lua
@@ -7,4 +7,41 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sqlfluff" },
+        pgsql = { "sqlfluff" },
+      },
+      formatters = {
+        sqlfluff = {
+          command = "sqlfluff",
+          -- dialect can use sqlite, postgres, mysql
+          -- more see https://docs.sqlfluff.com/en/stable/reference/dialects.html
+          args = { "format", "--dialect=sqlite", "-" },
+          stdin = true,
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+    },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    opts = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            procMacro = {
+              ignored = {
+                ["async-trait"] = vim.NIL,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
